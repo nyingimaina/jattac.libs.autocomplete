b71a0688440da1a43bd7b1298fa0492a5acf33b6
Nyingi <nyingi.maina@gmail.com>
2024-01-01 11:06:33 +0300

    0.0.4

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import IAutoCompleteOptionWrapper from './Data/IAutoCompleteOptionWrapper';
 import AutoComplete from './UI/AutoComplete';
 
+export { IAutoCompleteOptionWrapper };
 export default AutoComplete;
